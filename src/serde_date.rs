ec758f4cfd1dee3df62f824b507540ec1d78f06a
Eduardo Sabino <eduardo@amo.delivery>
2020-12-13 23:57:42 -0300

    change to naive date

--- src/serde_date.rs
+++ src/serde_date.rs
@@ -1,10 +1,10 @@
-use chrono::{DateTime, Utc, TimeZone};
+use chrono::NaiveDate;
 use serde::{self, Deserialize, Serializer, Deserializer};
 
 const FORMAT: &'static str = "%d/%m/%Y";
 
 pub fn serialize<S>(
-    date: &DateTime<Utc>,
+    date: &NaiveDate,
     serializer: S,
 ) -> Result<S::Ok, S::Error>
 
@@ -15,19 +15,12 @@ where
     serializer.serialize_str(&s)
 }
 
-// The signature of a deserialize_with function must follow the pattern:
-//
-//    fn deserialize<'de, D>(D) -> Result<T, D::Error>
-//    where
-//        D: Deserializer<'de>
-//
-// although it may also be generic over the output types T.
 pub fn deserialize<'de, D>(
     deserializer: D,
-) -> Result<DateTime<Utc>, D::Error>
+) -> Result<NaiveDate, D::Error>
 where
     D: Deserializer<'de>,
 {
     let s = String::deserialize(deserializer)?;
-    Utc.datetime_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
+    NaiveDate::parse_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
 }
